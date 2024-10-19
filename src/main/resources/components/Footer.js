5515333da1bfcdb0323a35f90d85ed6cef645913
sonalikad21 <kadsonali21@gmail.com>
2024-10-20 01:58:27 +0530

    Update Footer.js

---
+++ src/main/resources/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-dark text-light">
+      <p>&copy; Dev Rezaur</p>
+    </footer>
+  );
+}
+
+export default Footer;
